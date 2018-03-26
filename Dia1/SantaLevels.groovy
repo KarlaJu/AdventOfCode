a44d4daa51c3be4a03c7ecf6cde578b841e92fd3
karlaJu <karla@makingdevs.com>
2018-03-26 12:22:39 -0600

    Agregando dia 1 parte 2  de adventofcode

--- Dia1/SantaLevels.groovy
+++ Dia1/SantaLevels.groovy
@@ -1,23 +1,30 @@
 String levels = new File('/Users/makingdevs/Desktop/Programas/Santa floor.txt').text
+//String levels ="()())"
 
 int cont=0
-/*
-Forma 1 de resolverlo
+int nivel=0
+
+//Forma 1 de resolverlo
 
 for(int i=0; i<levels.size();i++)
 {
+
     if(levels[i]=="(")
+        cont++
+    else{
+        cont--
+        if(cont==-1)
+        return i+1}
+
+}
+
+//Forma más sencilla
+/*levels.each{ floor ->
+    if(floor=="(")
         cont++
     else
         cont--
 }*/
 
-//Forma más sencilla
-levels.each{ floor ->
-    if(floor=="(")
-        cont++
-    else
-        cont--
-}
-
 println cont
+println nivel
