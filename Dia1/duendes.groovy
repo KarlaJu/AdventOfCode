779aefdef6ce240df0d690c1fb4f0f6c1fa206c1
karlaJu <karla@makingdevs.com>
2018-03-27 11:24:38 -0600

    Subiendo dia 2 parte 1 de advent of code

--- Dia1/duendes.groovy
+++ Dia1/duendes.groovy
@@ -1,48 +1,29 @@
-/*
-operacion= """2x4x3
-              1x2x3"""
-
-for(int i=0;i<2;i++)
-{
-    lista=operacion.split('x')
-    lista.each { println it}
-    lista.collect{lista}
-// }*/
+String s = new File('regalos.txt').text
  
-def s = """2-3-4
-1-1-10"""
+//def s = """2x3x4
+//1x1x10"""
 
 def l = []
-int a, b, c, sum
+int a, b, c, sum, cont
 int p1=0, p2=0, p3=0
 
 s.eachLine{i->
-     l << i.split('-')
-     //println i.split('-')
-     //println i
-     //i.collect{s}
-    /* println l[0][0]
-     for(int q=0;q<2;q++){
-     a=l[0][0]
-     b=l[0][1]
-     c=l[0][2]
-     d=2*(a*b)
-     println d
-     }*/
-     //l.each{it.each{println it}}
-    
+     l << i.split('x')
 }
 l.each{a=it[0].toInteger()
        b=it[1].toInteger()
-       c=it[2].toInteger()       
+       c=it[2].toInteger()
+       lista= [a,b,c]       
        p1=2*(a*b)
-       println p1
        p2=2*(b*c)
-       println p2
        p3=2*(a*c)
-       println p3
+       if(lista.max()==a)
+       sum= p1+ p2+ p3 +(b*c)
+       if(lista.max()==b)
+       sum= p1+ p2+ p3 +(a*c)
+       if(lista.max()==c)
        sum= p1+ p2+ p3 +(a*b)
-       println sum
-       println "******"
+       cont=cont+ sum
       }
+println cont
  
