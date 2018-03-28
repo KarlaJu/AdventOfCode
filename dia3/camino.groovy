9bc6b70c2be6a6e328337020026c7d83490d8ef2
karlaJu <karla@makingdevs.com>
2018-03-28 10:07:30 -0600

    Subiendo dia 3 parte 1 de advent of code, inicio de programa, sin funcionar

---
+++ dia3/camino.groovy
@@ -0,0 +1,64 @@
+//String s = new File('regalos.txt').text
+
+//Variables
+/*def s = "<^v><"
+def coo=[x:0,y:0]
+def lc=[]
+def l =[]
+int cont, x=0, y=0
+def w
+def z
+List lista = []
+
+
+s.each{i->
+   l << i
+}
+println l
+
+
+l.each{ q -> 
+lista << q
+    /*if(s.each()==">")
+    {*/ 
+ /*   println q
+    println "****"
+        switch (q) {
+            case "<":
+                lc<< coo
+                w=((lc.size()).Integer)-1
+                println w
+                //z=[x:1,y:y[w]]
+                lc<< z
+                result = 'si funciona +1'
+                break
+            case ">":
+                result = 'si funciona+2'
+                break
+            case "^":
+                result = 'si funciona+3'
+                break
+            case "v":
+                result = 'Todo bien'
+                break
+            default:
+                result = 'Default'
+                break
+        }   
+        println result
+}*/
+/*l.each{a=it[0]
+       b=it[1]
+       c=it[2]
+       lista= [a,b,c]   */
+
+
+mapa = [x:[0,1,2,3,4,5], y:[0,1,2,3,4,5]]
+
+mapa.x << 6
+
+println mapa
+
+println mapa.y[6]?mapa.y[6]:(mapa.y << mapa.y[6-1])
+
+
