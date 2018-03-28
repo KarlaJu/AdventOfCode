e727c7a469748af917cc128830e5b679b47b5a52
karlaJu <karla@makingdevs.com>
2018-03-28 12:32:43 -0600

    Completando dia 3 parte 1 de advent of code

--- dia3/camino.groovy
+++ dia3/camino.groovy
@@ -1,64 +1,68 @@
-//String s = new File('regalos.txt').text
+String s = new File('caminos.txt').text
 
-//Variables
-/*def s = "<^v><"
-def coo=[x:0,y:0]
-def lc=[]
-def l =[]
-int cont, x=0, y=0
-def w
-def z
+/****Variables de pruebas***/
+/*def s = "<<^v>^"
+def s = "<<"*/
+def coo=[x:[0],y:[0]]
+
+int x=0, y=0
+def a
 List lista = []
 
+lista << "${coo.x[0]} ${coo.y[0]}"
+s.each{ q -> 
 
-s.each{i->
-   l << i
+    switch (q) {
+        case "<":
+            x=x-1
+            coo.x << x
+            a=coo.x.size()
+            coo.y[a-1]?coo.y[a-1]:(coo.y << coo.y[a-2])
+            lista << "${coo.x[a-1]} ${coo.y[a-1]}"
+            break
+        case ">":
+            x=x+1
+            coo.x << x
+            a=coo.x.size()
+            coo.y[a-1]?coo.y[a-1]:(coo.y << coo.y[a-2])
+            lista << "${coo.x[a-1]} ${coo.y[a-1]}"
+            break
+        case "^":
+            y=y+1
+            coo.y << y
+            a=coo.y.size()
+            coo.x[a-1]?coo.x[a-1]:(coo.x << coo.x[a-2])
+            lista << "${coo.x[a-1]} ${coo.y[a-1]}"
+            break
+        case "v":
+            y=y-1
+            coo.y << y
+            a=coo.y.size()
+            coo.x[a-1]?coo.x[a-1]:(coo.x << coo.x[a-2])
+            lista << "${coo.x[a-1]} ${coo.y[a-1]}"
+            break
+        default:
+            result = 'Default'
+            break
+    }  
 }
-println l
 
+println lista.unique().size()
 
-l.each{ q -> 
-lista << q
-    /*if(s.each()==">")
-    {*/ 
- /*   println q
-    println "****"
-        switch (q) {
-            case "<":
-                lc<< coo
-                w=((lc.size()).Integer)-1
-                println w
-                //z=[x:1,y:y[w]]
-                lc<< z
-                result = 'si funciona +1'
-                break
-            case ">":
-                result = 'si funciona+2'
-                break
-            case "^":
-                result = 'si funciona+3'
-                break
-            case "v":
-                result = 'Todo bien'
-                break
-            default:
-                result = 'Default'
-                break
-        }   
-        println result
-}*/
-/*l.each{a=it[0]
+/******Algunas pruebas con mapas******/
+
+/*mapa = [x:[0,1,1,1],y:[0,0,1,0]]
+println lista.unique()
+l.each{a=it[0]
        b=it[1]
        c=it[2]
        lista= [a,b,c]   */
 
 
-mapa = [x:[0,1,2,3,4,5], y:[0,1,2,3,4,5]]
+/*mapa = [x:[01,10,20,30,40,50], y:[01,10,20,30,40,50]]
+def variable
+//mapa.x << 6
+variable = mapa.x.size()
+println variable
 
-mapa.x << 6
-
-println mapa
-
-println mapa.y[6]?mapa.y[6]:(mapa.y << mapa.y[6-1])
-
-
+println mapa.y[variable]?mapa.y[variable]:(mapa.y << mapa.y[variable-1])*/
