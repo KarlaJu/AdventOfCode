79ee46a29959229a3fc294796f43a93c0a7f4e9b
karlaJu <karla@makingdevs.com>
2018-04-09 12:53:14 -0500

    Uso de split en String, iterando elementos dentro de un vector

---
+++ Dia2/using_strings.rs
@@ -0,0 +1,10 @@
+
+fn main(){
+  let s = "1x2x3";
+  let v: Vec<&str> = s.split("x").collect();
+  let numbers: Vec<i16> = v.into_iter().map(|x| x.parse::<i16>().unwrap()).collect();
+  
+  //println!("{:?}",v);
+  println!("{:?}",numbers);
+}
+
