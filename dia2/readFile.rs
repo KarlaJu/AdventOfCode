1d06cedd308fd3ca5430f94cb445f29c4459d722
karlaJu <karla@makingdevs.com>
2018-04-11 15:29:19 -0500

    Subiendo version en Rust aun no finalizada

--- dia2/readFile.rs
+++ dia2/readFile.rs
@@ -1,24 +1,45 @@
 //use std::env;
-use std::fs::File;
-use std::io::prelude::*;
-
-fn main() {
+/*use std::fs::File;
+use std::io::prelude::*;*/
+////////use std::mem;
+/*fn main() {
     // --snip--
     //println!("In file {}", filename);
 
     let mut f = File::open("texto.txt").expect("file not found");
+	let lista=[1,2,3];
+    let mut contents = String::new();
+    
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
+    
+    for i in 0..3 {
+    	println!("{}",lista[i]);
+    }
+*/
+//use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    //Leer archivos
+    let mut f = File::open("text.txt").expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}/*
+use std::string::String;
+fn main() {
+    let s = String::from("initial contents");
+    println!("{}", s);
+}*/
 
-    let lista=[1,2,3];
-    for i in 0..3 {
-    	println!("{}",lista[i]);
-    }
-}
 
 
 
