2b2e39022cdc7efc5505a6b4fea85373cf498f3d
karlaJu <karla@makingdevs.com>
2018-04-18 13:09:34 -0500

    Quitando variables sin usar, modificando validaciones

--- dia5/Cadenas.groovy
+++ dia5/Cadenas.groovy
@@ -1,40 +1,35 @@
-def alfa=[]
-def l=[],l2=[]
-def contCad=0, conta=0
-def n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13
-def n14, n15, n16, n17
+//def alfa=[]
+def l=[],l2=[],l3=[]
+def contCad=0, conta=0, contadorCade=0
+
 def coo=[x:[0],y:[0]]
 def cadena= """xaabieez
 qwer"""
 
+/******Ayuda para crear arreglos
 println ('a'..'z')
-
 alfa=('a'..'z')*.bytes.flatten()
-println(alfa)
+println(alfa)****/
 
 cadena.eachLine{i ->
-    l<< i.bytes.collect{it as Integer}
-        println("_______")
-        println(i)
-        
+    l<< i//.bytes.collect{it as Integer}
         
     if(i.contains("a")||i.contains("e")||i.contains("i")||i.contains("o")||i.contains("u"))
     {
         def contador=0, cont2=0
-        int num1=0, num2=0, num
-        println(i.size())
+        def num1, num2, num
         for(int j=0; j<=i.size(); j++)
         {
-        println("Este es el valor del j: "+i[j])
+        //println("Este es el valor del j: "+i[j])
              if(i[j]=="a"||i[j]=="e"||i[j]=="i"||i[j]=="o"||i[j]=="u")
              {
              contador++
-             //println("el contador: "+contador)
+             //println("el contador: "+contador)    Prueba para imprimir el contador de vocales
              if(contador>=3)
              {
                  for(int w=0; w<=i.size(); w++)
                  {
-                     println ("Este es el valor i: "+i[w]+" este es el valor siguiente "+i[w+1])
+                     //println ("Este es el valor i: "+i[w]+" este es el valor siguiente "+i[w+1])
                         if(i[w]==i[w+1])
                         {
                             cont2++
@@ -42,30 +37,33 @@ cadena.eachLine{i ->
                             {
                                 for(int x=0; x<i.size(); x++)
                                 {
+                                    //l3=l.bytes.flatten()
                                     
                                     num1=i[x]
-                                    num2=i[x+1]
-                                    
+                                    num2=i[x+1] 
+                                    println("El uno: "+num1+" El dos: " +num2)                               
                                     l2=[num1,num2]
-                                    if(l2.max()==num1)
+                                    l3=l2.bytes.flatten()
+                                    println(l3)
+                                    if(l3.max()==num1)
                                     {   
-                                        num=num1-num2
-                                        println("Este es el numero uno: "+num1+" Este es el numero dos: "+num2)
-                                        println(num)
-                                        /*if(num!=1)
+                                        num=l3[0]-l3[1]
+                                        println(num)//si se confirma que la cadena tiene una serie de caracteres que no valen
+                                        if(num==1)//Entonces no se cuenta
                                         {
-                                            println("Todo bien")
-                                        }*/
-                                        // println("Este es el numero uno: "+num1+" Este es el numero dos: "+num2)
+                                           conta++ 
+                                           println(conta)
+                                        }
                                     }
                                     else
                                     {
-                                        num=num2-num1
-                                        println("Este es el numero uno de lo segundo: "+num1+" Este es el numero dos: "+num2)
+                                        num=l3[1]-l3[0]
+                                        //println("Este es el numero uno de lo segundo: "+num1+" Este es el numero dos: "+num2)
                                         println(num)
-                                        if(num!=1)
+                                        if(num==1)
                                         {
                                            conta++ 
+                                           println(conta)
                                         }
                                     }
                                 }
@@ -75,5 +73,12 @@ cadena.eachLine{i ->
                 }
             }   
         }
-    }   
+    } 
+    if(conta>0)
+    {
+        contadorCade++
+        
+    }
+
 }
+println("Prueba"+contadorCade)
