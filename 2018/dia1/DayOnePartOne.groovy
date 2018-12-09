5fb6e72856666e300d3357232c085a56b25e5526
KarlaJu <karla@makingdevs.com>
2018-12-08 22:10:38 -0600

    Agregando input completo para resolver el problema

--- 2018/dia1/DayOnePartOne.groovy
+++ 2018/dia1/DayOnePartOne.groovy
@@ -1,9 +1,10 @@
-//String listOfNumbers = new File('/mnt/c/projects/AdventOfCode/2018/Dia1/frecuncy.txt').text
-String listOfNumbers = "-1\n-16"
+String listOfNumbers = new File('frecuency.txt').text
+//String listOfNumbers = "-1\n-16"
 
   def sum = 0
 	listOfNumbers.eachLine{number ->
-  println number 
-    sum = sum + number.toInteger()
+    println number
+    sum = sum + (number.toInteger())
+    println ("Suma: ${sum}")
   }
-  println sum
+  println "Esta es la total ${sum}"
