1da5e1e0fb0ed259209e3e7dbfdb517f3883259e
KarlaJu <karla@makingdevs.com>
2018-12-08 22:42:10 -0600

    Identación del código

--- 2018/dia1/DayOnePartOne.groovy
+++ 2018/dia1/DayOnePartOne.groovy
@@ -1,10 +1,10 @@
 String listOfNumbers = new File('frecuency.txt').text
 //String listOfNumbers = "-1\n-16"
 
-  def sum = 0
-	listOfNumbers.eachLine{number ->
-    println number
-    sum = sum + (number.toInteger())
-    println ("Suma: ${sum}")
-  }
-  println "Esta es la total ${sum}"
+def sum = 0
+listOfNumbers.eachLine{number ->
+  println number
+  sum = sum + (number.toInteger())
+  println ("Suma: ${sum}")
+}
+println "Esta es la total ${sum}"
